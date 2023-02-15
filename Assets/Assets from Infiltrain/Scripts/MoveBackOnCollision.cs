c552aa10c1aa27325122a0399137f2b3d94e6612
123kc321 <121979765+123kc321@users.noreply.github.com>
2023-02-15 12:48:07 -0800

    Hierarchy organization

--- Assets/Assets from Infiltrain/Scripts/MoveBackOnCollision.cs
+++ Assets/Assets from Infiltrain/Scripts/MoveBackOnCollision.cs
@@ -4,7 +4,18 @@ using UnityEngine;
 
 public class MoveBackOnCollision : MonoBehaviour
 {
-    public float detachSpeed = 100;
+    public float speed = 100f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
     void OnCollisionEnter(Collision col)
     {
         if (col.gameObject.tag == "Enemy")
@@ -17,8 +28,9 @@ public class MoveBackOnCollision : MonoBehaviour
     {
         while (true)
         {
-            transform.Translate(Vector3.back * Time.deltaTime * detachSpeed);
+            transform.Translate(Vector3.back * Time.deltaTime * speed);
             yield return new WaitForSeconds(0.01f); // add a delay between each movement
         }
     }
 }
+
