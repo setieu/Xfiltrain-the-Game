05cf4447748a8ae4c57367a73d272b7d12f80b94
123kc321 <121979765+123kc321@users.noreply.github.com>
2023-03-08 13:22:45 -0800

    train crash audios

--- Assets/Assets from Infiltrain/Scripts/MoveBackOnCollision.cs
+++ Assets/Assets from Infiltrain/Scripts/MoveBackOnCollision.cs
@@ -6,15 +6,24 @@ public class MoveBackOnCollision : MonoBehaviour
 {
     private float leftBound = -100;
     public float speed = 100f;
+    public int detachD = 0;
+
+    private AudioSource audioSource; // audio source component
+    public List<AudioClip> DetachmentAudio; // list of audio clips to choose from for Detachment
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (detachD == 1)
+        {
+            PlayRandomDetachmentAudio();
+        }
         //Destroy flatcar when out of bounds
         if (transform.position.x < leftBound && gameObject.CompareTag("Ground"))
         {
@@ -26,6 +35,7 @@ public class MoveBackOnCollision : MonoBehaviour
         if (other.gameObject.CompareTag("Enemy"))
         {
             StartCoroutine(MoveBack());
+            detachD++;
         }
     }
 
@@ -36,6 +46,13 @@ public class MoveBackOnCollision : MonoBehaviour
             transform.Translate(Vector3.back * Time.deltaTime * speed);
             yield return new WaitForSeconds(0.01f); // add a delay between each movement
         }
+    }
+
+    void PlayRandomDetachmentAudio()
+    {
+        int randomIndex = Random.Range(0, DetachmentAudio.Count); // choose a random index within the list
+        audioSource.clip = DetachmentAudio[randomIndex]; // set the audio source's clip to the chosen audio clip
+        audioSource.Play(); // play the audio
     }
 }
 
