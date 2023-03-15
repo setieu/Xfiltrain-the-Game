dfaf71be394a2fb072dd5582287f6a1dec0f0cec
123kc321 <121979765+123kc321@users.noreply.github.com>
2023-03-15 12:21:46 -0700

    invereted stuff movement cam

--- Assets/Assets from Infiltrain/Scripts/PlayerController.cs
+++ Assets/Assets from Infiltrain/Scripts/PlayerController.cs
@@ -162,10 +162,21 @@ public class PlayerController : MonoBehaviour
         if (gameManager.gameActive)
         {
             // Get the player movement direction
-            Vector3 movementDirection = new Vector3(verticalInput, 0.0f, -1.5f * horizontalInput);
+
+                Vector3 movementDirection = new Vector3(verticalInput, 0.0f, -1.5f * horizontalInput);
+
+
 
             // Normalize the movement direction vector
-            movementDirection = movementDirection.normalized;
+            if (followPlayer.isFlipped == 1)
+            {
+                 movementDirection = movementDirection.normalized;
+            }
+            else if (followPlayer.isFlipped == 2)
+            {
+                movementDirection = -movementDirection.normalized;
+            }
+
 
             // Translate the player's position based on the movement direction
             playerRb.transform.Translate(movementDirection * speed * forceMultiplier * Time.deltaTime, Space.World);
