310673b1c45ee359f240c9cd331b1d2cb772c8d5
123kc321 <121979765+123kc321@users.noreply.github.com>
2023-03-21 13:13:19 -0700

    WaveSpawn Audio

--- Assets/Assets from Infiltrain/Scripts/PogHider.cs
+++ Assets/Assets from Infiltrain/Scripts/PogHider.cs
@@ -1,23 +1,26 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PogHider : MonoBehaviour
 {
+    private AudioSource audioSource;
     public GameObject objectToSpawn;
     public float spawnDelay = 2f;
     public float waveDelay = 2f;
     public int waveSize = 1;
     public int maxPogs = 10;
-
+    public List<AudioClip> WaveAudio; // list of audio clips to choose from for hogrider dying
     private float lastWaveTime;
     private int objectsSpawnedInCurrentWave;
     private GameManager gameManager;
     private SpawnManager spawnManager;
     //GameObject modeS = GameObject.Find("Modes");
     private Difficulty difficultY;
-
+    
     private void Start()
     {
+        audioSource = GetComponent<AudioSource>();
         gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
         spawnManager = GameObject.Find("SpawnManager").GetComponent<SpawnManager>();
         //  Difficulty difficultyScript = modeS.GetComponent<Difficulty>();
@@ -69,12 +72,20 @@ public class PogHider : MonoBehaviour
                 {
                     GameObject spawnedObject = Instantiate(objectToSpawn, spawnPosition, Quaternion.identity);
                     objectsSpawnedInCurrentWave++;
+                    PlayRandomWaveAudio();
                     Debug.Log("Spawning wave " + (int)waveSize);
                 }
 
                 yield return new WaitForSeconds(spawnDelay);
 
             }
+        }
+
+        void PlayRandomWaveAudio()
+        {
+            int randomIndex = Random.Range(0, WaveAudio.Count); // choose a random index within the list
+            audioSource.clip = WaveAudio[randomIndex]; // set the audio source's clip to the chosen audio clip
+            audioSource.Play(); // play the audio
         }
     }
 }
