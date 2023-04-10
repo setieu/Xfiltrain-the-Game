db31da94e31cd13704f56de821c865565cf80b3f
123kc321 <121979765+123kc321@users.noreply.github.com>
2023-04-10 15:47:15 -0700

    randomrange

--- Assets/Assets from Infiltrain/Scripts/Bossman.cs
+++ Assets/Assets from Infiltrain/Scripts/Bossman.cs
@@ -148,6 +148,7 @@ public class Bossman : MonoBehaviour
 
 
                 // Wait for the next spawn interval
+                spawnInterval = Random.Range(0.25f, 3f);
                 yield return new WaitForSeconds(spawnInterval);
                 firstRocket = false;
             }
