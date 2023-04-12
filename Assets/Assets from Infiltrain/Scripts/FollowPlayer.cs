a9d731da5d2cc54f88b965da9921eed0a25a150c
galei1 <121979486+galei1@users.noreply.github.com>
2023-04-12 09:14:10 -0700

    working followplayer

--- Assets/Assets from Infiltrain/Scripts/FollowPlayer.cs
+++ Assets/Assets from Infiltrain/Scripts/FollowPlayer.cs
@@ -5,18 +5,22 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 offsetX = new Vector3(-8, 16, 0);
-    private Vector3 offsettwo = new Vector3(-12, 20, 0);
+    private Vector3 offsetX = new Vector3(8, 16, 0);
+    private Vector3 offsettwo = new Vector3(12, 20, 0);
     private PlayerController playerController;
     public float rotationSpeed = 2500;
-    public int isFlipped = 1; // Flag to check if object is already flipped
+    public int isFlipped = 2; // Flag to check if object is already flipped
     private GameManager gameManager;
     public AudioSource audioSource;
+    public AudioClip audioClip;
+    public AudioClip secondAudioClip;
+    public AudioClip thirdAudioClip;
     public AudioClip lagtrain;
     public AudioClip gsc;
     public AudioClip pillyman;
     public AudioClip chill;
     public AudioClip journey;
+    public bool rotateonce = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -30,32 +34,32 @@ public class FollowPlayer : MonoBehaviour
         gameManager.modeeE = 7;
 
     }
-     void Update()
+    void Update()
     {
         if (gameManager.modeeE == 0)
         {
-            audioSource.volume = 1.0f;
+            audioSource.volume = .4f;
             audioSource.Pause();
             audioSource.clip = chill;
             audioSource.Play();
         }
         if (gameManager.modeeE == 1)
         {
-            audioSource.volume = 1.0f;
+            audioSource.volume = .4f;
             audioSource.Pause();
             audioSource.clip = lagtrain;
             audioSource.Play();
         }
         if (gameManager.modeeE == 2)
         {
-            audioSource.volume = 1.0f;
+            audioSource.volume = .4f;
             audioSource.Pause();
             audioSource.clip = lagtrain;
             audioSource.Play();
         }
         if (gameManager.modeeE == 3)
         {
-            audioSource.volume = 1.0f;
+            audioSource.volume = .4f;
             audioSource.Pause();
             audioSource.clip = lagtrain;
             audioSource.Play();
@@ -67,7 +71,7 @@ public class FollowPlayer : MonoBehaviour
             audioSource.Play();
             //Debug.Log("secondaudio");
         }
-        if(gameManager.modeeE == 5)
+        if (gameManager.modeeE == 5)
         {
             audioSource.volume = .4f;
             audioSource.Pause();
@@ -76,7 +80,7 @@ public class FollowPlayer : MonoBehaviour
         }
         if (gameManager.modeeE == 7)
         {
-            audioSource.volume = 1.0f;
+            audioSource.volume = .4f;
             audioSource.Pause();
             audioSource.clip = journey;
             audioSource.Play();
@@ -89,12 +93,18 @@ public class FollowPlayer : MonoBehaviour
         // Camera Offset
         //if (playerController.isAlive == true)
         {
-            if(gameManager.modeeE != 5)
+            if (gameManager.modeeE != 5)
             {
                 if (gameManager.gameActive)
                 {
                     if (playerController.isOnDead == false)
                     {
+                        if (rotateonce)
+                        {
+                            transform.rotation = Quaternion.Euler(50f, transform.rotation.eulerAngles.y + 180f, 0f);
+                            rotateonce = false;
+                        }
+
                         transform.position = player.transform.position + offsetX;
                     }
                 }
@@ -103,12 +113,18 @@ public class FollowPlayer : MonoBehaviour
                     transform.position = transform.position;
                 }
             }
-            else if(gameManager.modeeE == 5)
+            else if (gameManager.modeeE == 5)
             {
                 if (gameManager.gameActive)
                 {
                     if (playerController.isOnDead == false)
                     {
+                        if (rotateonce)
+                        {
+                            transform.rotation = Quaternion.Euler(50f, transform.rotation.eulerAngles.y + 180f, 0f);
+                            rotateonce = false;
+                        }
+
                         transform.position = player.transform.position + offsettwo;
                     }
                 }
@@ -121,7 +137,7 @@ public class FollowPlayer : MonoBehaviour
 
 
             // Rotate the camera clockwise about the y-axis while 'z' key is pressed
-            if(gameManager.modeeE != 5)
+            if (gameManager.modeeE != 5)
             {
                 if (gameManager.gameActive == true)
                 {
@@ -129,7 +145,7 @@ public class FollowPlayer : MonoBehaviour
                     {
                         // Flip the object 180 degrees about the y-axis
                         transform.rotation = Quaternion.Euler(50f, transform.rotation.eulerAngles.y + 180f, 0f);
-                        offsetX = new Vector3(8, 16, 0);
+                        offsetX = new Vector3(-8, 16, 0);
                         // Update the flag to reflect the current state of the object
                         isFlipped = 2;
                     }
@@ -137,13 +153,13 @@ public class FollowPlayer : MonoBehaviour
                     {
                         // Flip the object 180 degrees about the y-axis
                         transform.rotation = Quaternion.Euler(50f, transform.rotation.eulerAngles.y + 180f, 0f);
-                        offsetX = new Vector3(-8, 16, 0);
+                        offsetX = new Vector3(8, 16, 0);
                         // Update the flag to reflect the current state of the object
                         isFlipped = 1;
                     }
                 }
             }
-            else if(gameManager.modeeE == 5)
+            else if (gameManager.modeeE == 5)
             {
                 if (gameManager.gameActive == true)
                 {
