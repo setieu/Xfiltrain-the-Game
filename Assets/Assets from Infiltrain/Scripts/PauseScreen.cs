a60c6e5a5d31fa0b003298ffd8fc2adae1877016
galei1 <121979486+galei1@users.noreply.github.com>
2023-04-25 15:47:40 -0700

    p to pause

--- Assets/Assets from Infiltrain/Scripts/PauseScreen.cs
+++ Assets/Assets from Infiltrain/Scripts/PauseScreen.cs
@@ -16,7 +16,7 @@ public class PauseScreen : MonoBehaviour
     }
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
         {
             if (!isPaused)//notpaused then will pause
             {
