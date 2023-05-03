9282c8f5e4ed4c4582cb0922d94130305ac173e9
galei1 <121979486+galei1@users.noreply.github.com>
2023-05-03 13:27:48 -0700

    blinking object fix

--- Assets/Assets from Infiltrain/Scripts/GLowEffect.cs
+++ Assets/Assets from Infiltrain/Scripts/GLowEffect.cs
@@ -11,42 +11,16 @@ public class GLowEffect : MonoBehaviour
     public GameObject glower;
     public GameManager gameManager;
 
-    private Renderer rend;
-    private bool isEffectOn = true;
 
     void Start()
     {
-        // Get the Renderer component of the GameObject
-        rend = GetComponent<Renderer>();
 
     }
 
     void Update()
     {
-        float redValue = Mathf.Sin(Time.time * fadeSpeed) * redIntensity;
 
-        // Set the color of the material to the new red color
-        rend.material.color = new Color(redValue, 0.0f, 0.0f, 1.0f);
-        // Toggle the effect on/off when the H key is pressed
-        if (Input.GetKeyDown(KeyCode.H))
-        {
-            yeeyee = !yeeyee;
-        }
 
-        if (gameManager.gameActive == true && yeeyee == true)
-        {
-            glower.SetActive(true);
-        }
-        else
-        {
-            glower.SetActive(false);
-        }
 
-        // If the effect is on, update the red color and fade effect
-        if (isEffectOn)
-        {
-            // Calculate the new red color based on the current time
-
-        }
     }
 }
