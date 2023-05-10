11e139ec403d1c64a34be4ca9838b2d3883d77da
galei1 <121979486+galei1@users.noreply.github.com>
2023-05-10 12:05:00 -0700

    Update CommitInflationFraud.cs

--- Assets/Assets from Infiltrain/Scripts/CommitInflationFraud.cs
+++ Assets/Assets from Infiltrain/Scripts/CommitInflationFraud.cs
@@ -68,7 +68,7 @@ public class CommitInflationFraud : MonoBehaviour
         //commit//commit
         //commit//commit
         //commit//commit//commit
-        //commit
+        //commit//commit
         //commit
         //commit
         //commit
